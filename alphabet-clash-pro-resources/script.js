173705c60ee0262c12b1917f8268b5b7a4a0edf7
Naimur00 <aminaim4u@gmail.com>
2024-02-15 01:57:39 +0600

    random Alphabet and key color js added

--- alphabet-clash-pro-resources/script.js
+++ alphabet-clash-pro-resources/script.js
@@ -1,18 +1,30 @@
 document.getElementById('play-now').addEventListener('click', function(){
+    // Add first screen
     const firstScreen = document.getElementById('first-screen');
     firstScreen.classList.add('hidden');
 
+    // Hide second screen
     const secondScreen = document.getElementById('second-screen');
     secondScreen.classList.remove('hidden');
 
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabetArray = alphabet.split('');
-    const randomNum = parseInt(Math.random() * 25);
-    const randomAlphabet = alphabetArray[randomNum];
+    // Random Alphabet
+    const randomLetter = randomAlphabet();
+    const randomLetterOnScreen = document.getElementById('random-letter');
+    randomLetterOnScreen.innerText = randomLetter;
 
-    const randomLetter = document.getElementById('random-letter');
-    randomLetter.innerText = randomAlphabet;
+    // key button background color change
+    const keyButton = document.getElementById(randomLetter);
+    keyButton.style.background = 'orange';
 
 });
 
 
+
+// For random Alphabet generator
+function randomAlphabet() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetArray = alphabet.split('');
+    const randomNum = parseInt(Math.random() * 25);
+    const randomAlphabet = alphabetArray[randomNum];
+    return randomAlphabet;    
+}
